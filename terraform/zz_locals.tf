ff3f41715000ef5cbdf78bbad1a02a0a69adc774
Simon Weald <glitchcrab-github@simonweald.com>
2022-06-04 22:01:13 +0100

    adjust instance IP

--- terraform/zz_locals.tf
+++ terraform/zz_locals.tf
@@ -3,7 +3,7 @@ locals {
   snippet_root_dir   = "/mnt/pve/cloudinit"
   snippet_dir        = "snippets"
   primary_ip_gateway = cidrhost(var.net0_network_cidr, 1)
-  ip_offset          = 4
+  ip_offset          = 5
   instance_name      = "actions-runner"
   pm_host_address    = data.vault_generic_secret.terraform_generic.data["host"]
 }
